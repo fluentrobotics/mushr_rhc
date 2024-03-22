8602f22359bb12fb05787391ca88cc35610b6e31
Jeeho Ahn <jeeho@umich.edu>
2024-03-22 03:15:25 +0000

    tuning

--- src/mpc.py
+++ src/mpc.py
@@ -174,9 +174,9 @@ class ModelPredictiveController(BaseController):
         error_th = ref_pose.th - pose[2]
 
         #arbitrary Kx
-        Kx = 0.45
+        Kx = 0.42
         #keeping distance
-        kd = 0.1
+        kd = 0.12
         tracking_speed = self.speed*math.cos(error_th) + Kx * (error_x-kd) #kanayama linear velocity
         #print(tracking_speed)
         #for analysis
